c4e48d847ff970a289de06a600b320281f12f9a1
Abuthahir <167390105+Abuthahir-M@users.noreply.github.com>
2024-04-20 16:38:22 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,22 +1,16 @@
 function searchWord() {
     var word = document.getElementById('word').value;
     var capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/search', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                var result = response.result;
-                var output = capitalizedWord + ': ' + result;
-                document.getElementById('definition').innerText = output;
-                document.getElementById('word').value = '';
-            } else {
-                console.error('Request failed with status:', xhr.status);
-            }
+    $.ajax({
+        type: 'POST',
+        url: '/search',
+        data: {word: word},
+        success: function(response) {
+            let result = response.result;
+            let output = capitalizedWord + ': ' + result;
+            document.getElementById('definition').innerText = output;
+            document.getElementById('word').value = '';
         }
-    };
-    xhr.send(JSON.stringify({word: word}));
+    
+    });
 }
